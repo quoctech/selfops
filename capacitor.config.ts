db0024aad057efe1d49aa2e7c6fd4c4d9bb58382
Quoc Tran <tranquocbn@gmail.com>
2026-01-11 12:03:49 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Self Ops',
+  webDir: 'www'
+};
+
+export default config;
