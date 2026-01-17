9f36223b2693f34faba451849eb10e41e7b0786d
Quoc Tran <tranquocbn@gmail.com>
2026-01-17 23:02:05 +0700

    Update version

--- src/app/features/settings/settings.page.ts
+++ src/app/features/settings/settings.page.ts
@@ -257,7 +257,7 @@ import { NotificationService } from 'src/app/core/services/notification.service'
             (mousedown)="startPress()"
             (mouseup)="endPress()"
           >
-            v1.3.0 (Build 2026.01)
+            v1.3.1 (Build 2026.01)
           </div>
         </div>
       </div>
